525425190c7d5cce5f5fad8b3db72f7e5e3cd1a2
Todd Bowden <toddhokie@gmail.com>
2022-02-28 11:05:14 -0500

    add macCatalyst

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "TBAPI",
     platforms: [
         .macOS(.v12),
-        .iOS(.v14)
+        .iOS(.v14),
+        .macCatalyst(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
